056f6944dc05d56521f5139bb8cfcb8f20d32927
Antu_Das <antuchandradasp9@gmail.com>
2022-11-09 06:40:05 -0800

    All service section added

--- src/Pages/AllServices/ServiceCard.js
+++ src/Pages/AllServices/ServiceCard.js
@@ -6,17 +6,19 @@ const ServiceCard = ({ service }) => {
 
 
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={image_url} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {service_name}
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>{details}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+            <div>
+                <div className="card lg:card-side bg-base-100 shadow-xl">
+                    <div className='lg:w-1/3'>
+                        <figure><img className='rounded-lg' src={image_url} alt="Album" /></figure>
+                    </div>
+                    <div className="card-body lg:w-2/3">
+                        <h2 className="card-title">{service_name}</h2>
+                        <p>{details}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Listen</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
